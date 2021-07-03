2f89484986381e1c116df690c2393ce2252f957e
Cyril Shaji <cyrilshaji38@gmail.com>
2021-07-04 00:15:44 +0530

    route to teacher dash

--- SparkApp/login/routes.py
+++ SparkApp/login/routes.py
@@ -1,3 +1,4 @@
+from SparkApp.dashboard.student.routes import student_page
 from flask import render_template, redirect, url_for, flash
 from SparkApp.login.forms import SigninForm
 from SparkApp.register.models import User
@@ -12,7 +13,10 @@ def login_page():
         attempted_user = User.query.filter_by(username=form1.username.data).first()
         if attempted_user and attempted_user.check_password_correction(attempted_password=form1.password.data):   # To ensure username and password match according to database.
             login_user(attempted_user)
-            return redirect(url_for('test_page'))   # Redirect user to page with a sample test paper.
+            if(attempted_user.acctype == 1):
+                return redirect(url_for('teacher_page'))
+            else:
+                return redirect(url_for('student_page'))
         else:
             flash('Username and password do not match! Please try again', category='danger')
     return render_template('login/login.html', form=form1)
