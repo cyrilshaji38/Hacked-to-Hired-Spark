31c2da6073324e48031f08247f907fc835bcac13
Cyril Shaji <cyrilshaji38@gmail.com>
2021-07-04 13:58:12 +0530

    profile pic on student dash

--- SparkApp/dashboard/student/routes.py
+++ SparkApp/dashboard/student/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, url_for
 from SparkApp import app
+from flask_login import current_user
 
 
 @app.route("/student")
 def student_page():
-    return render_template('dashboard/student/student.html')
+    profile = url_for('static', filename='profile_pics/' + current_user.profile)
+    return render_template('dashboard/student/student.html', profile=profile)
