2b33a7381b50c9ce533a050245f8b2ccff8d0df2
Cyril Shaji <cyrilshaji38@gmail.com>
2021-07-06 08:59:44 +0530

    student-teacher relationship

--- SparkApp/dashboard/teacher/routes.py
+++ SparkApp/dashboard/teacher/routes.py
@@ -1,15 +1,27 @@
+from SparkApp.register.models import Students
 from SparkApp.dashboard.teacher.forms import AddStudentForm
 from flask import render_template, url_for
 from werkzeug.utils import redirect
-from SparkApp import app
+from SparkApp import app, db
 from flask_login import current_user
+from SparkApp.register.models import User
 
 
 @app.route("/teacher", methods=['GET','POST'])
 def teacher_page():
     if current_user.is_authenticated:
         profile = url_for('static', filename='profile_pics/' + current_user.profile)
-        from4 = AddStudentForm()
-        return render_template('dashboard/teacher/teacher.html', profile=profile, form=from4)
+        form4 = AddStudentForm()
+        if form4.validate_on_submit():
+            if(User.query.filter_by(username=form4.username.data).first()):
+                if(Students.query.filter_by(username=form4.username.data).first()):
+                    print("already your student!")
+                else:
+                    student_to_add = Students(username=form4.username.data,teacher=current_user.id)
+                    db.session.add(student_to_add)
+                    db.session.commit()
+            else:
+                print("no such student")
+        return render_template('dashboard/teacher/teacher.html', profile=profile, form=form4)
     else:
         return redirect(url_for('login_page'))
--- SparkApp/register/models.py
+++ SparkApp/register/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):   # Table in database with all registered user'
     profile = db.Column(db.String(20), nullable=False, default='default.png')
     password = db.Column(db.String(length=60), nullable=False)
     acctype = db.Column(db.Integer(), nullable=False)
+    students = db.relationship('Students', backref='my_teacher', lazy=True)
 
     @property
     def epassword(self):
@@ -27,3 +28,9 @@ class User(db.Model, UserMixin):   # Table in database with all registered user'
 
     def check_password_correction(self, attempted_password):   # Compares attempted password with hash encoded password in database for user to login.
         return bcrypt.check_password_hash(self.password, attempted_password)
+
+
+class Students(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=True, unique=True)
+    teacher = db.Column(db.Integer(), db.ForeignKey('user.id'))
