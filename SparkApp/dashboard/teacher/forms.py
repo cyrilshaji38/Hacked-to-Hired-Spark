e903143debbb51958c37ac521c7af62f3fb6ec83
Cyril Shaji <cyrilshaji38@gmail.com>
2021-07-06 18:47:08 +0530

    merged main into it

---
+++ SparkApp/dashboard/teacher/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class AddStudentForm(FlaskForm):   # Form adding new student.
+    style={'style': 'font-size: 15px'}
+    style1={'style': 'font-size: 15px', 'readonly': True}
+    username = StringField(label='Student Username:', validators=[DataRequired()],render_kw=style)
+    submit = SubmitField(label='ADD',render_kw=style1)
--- SparkApp/dashboard/teacher/routes.py
+++ SparkApp/dashboard/teacher/routes.py
@@ -1,13 +1,29 @@
+from flask.helpers import flash
+from SparkApp.register.models import Students
+from SparkApp.dashboard.teacher.forms import AddStudentForm
 from flask import render_template, url_for
 from werkzeug.utils import redirect
-from SparkApp import app
+from SparkApp import app, db
 from flask_login import current_user
+from SparkApp.register.models import User
 
 
-@app.route("/teacher")
+@app.route("/teacher", methods=['GET','POST'])
 def teacher_page():
     if current_user.is_authenticated:
+        my_students = Students.query.filter_by(teacher=current_user.id) 
         profile = url_for('static', filename='profile_pics/' + current_user.profile)
-        return render_template('dashboard/teacher/teacher.html', profile=profile)
+        form4 = AddStudentForm()
+        if form4.validate_on_submit():
+            if User.query.filter_by(username=form4.username.data).first():
+                if Students.query.filter_by(username=form4.username.data).first():
+                    flash("Student already assigned to a teacher!", category='danger')
+                else:
+                    student_to_add = Students(username=form4.username.data,teacher=current_user.id)
+                    db.session.add(student_to_add)
+                    db.session.commit()
+            else:
+                flash("No such student!", category='danger')
+        return render_template('dashboard/teacher/teacher.html', profile=profile, form=form4, my_students=my_students)
     else:
         return redirect(url_for('login_page'))
