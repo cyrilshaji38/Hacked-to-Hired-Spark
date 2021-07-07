1722f562ab205c9eee897aaea361e4fa8970ef58
Nikita Mathew <nikitmathew2000@gmail.com>
2021-07-07 15:25:26 +0530

    fixed signout button

--- SparkApp/assesment/routes.py
+++ SparkApp/assesment/routes.py
@@ -2,16 +2,20 @@ from flask import render_template, redirect, url_for
 from SparkApp.skills import analyze_skill
 from SparkApp.assesment.forms import Answers
 from SparkApp import app
-
+from flask_login import current_user
 
 @app.route("/assesment",methods = ['GET','POST'])
 def assesment_page():
-    form3 = Answers()
-    form3.ans_list = [[form3.a1.data,form3.a2.data,form3.a3.data,form3.a4.data,form3.a5.data,form3.a6.data]]
-    if form3.validate_on_submit():
-        prediction = analyze_skill.lacking_skill(form3.ans_list)   # ML function returns the skill that the student lacks based on test answers.
-        if(prediction == "Critical thinking "):
-            return redirect(url_for('critical_thinking_page'))   # Redirect user to page with game for improving critical thinking skills.
-        else:
-            return redirect(url_for('communication_page'))   # Redirect user to page with game for improving communication skills.
-    return render_template('assesment/sample_assesment.html', form=form3)
+    if current_user.is_authenticated:
+        form3 = Answers()
+        form3.ans_list = [[form3.a1.data,form3.a2.data,form3.a3.data,form3.a4.data,form3.a5.data,form3.a6.data]]
+        if form3.validate_on_submit():
+            prediction = analyze_skill.lacking_skill(form3.ans_list)   # ML function returns the skill that the student lacks based on test answers.
+            if(prediction == "Critical thinking "):
+                return redirect(url_for('critical_thinking_page'))   # Redirect user to page with game for improving critical thinking skills.
+            else:
+                return redirect(url_for('communication_page'))   # Redirect user to page with game for improving communication skills.
+        return render_template('assesment/sample_assesment.html', form=form3)
+    else:
+        return redirect(url_for('login_page'))
+    
--- SparkApp/skills/routes.py
+++ SparkApp/skills/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template
+from flask import render_template,redirect, url_for
 from SparkApp import app
+from flask_login import current_user
 
 
 @app.route("/communication")
 def communication_page():
-    return render_template('skills/communication.html')
-
+    if current_user.is_authenticated:
+        return render_template('skills/communication.html')
+    else:
+        return redirect(url_for('login_page'))
 
 @app.route("/critical_thinking")
 def critical_thinking_page():
-    return render_template('skills/critical_thinking.html')
+    if current_user.is_authenticated:
+        return render_template('skills/critical_thinking.html')
+    else:
+        return redirect(url_for('login_page'))
